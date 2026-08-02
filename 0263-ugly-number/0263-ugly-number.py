06f7eb4ddfb890e44599a241425877de810156af
malolan26-cmd <malolan26@gmail.com>
2026-08-02 17:24:41 -0400

    Time: 0 ms (100%), Space: 19.3 MB (28.98%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -2,7 +2,7 @@ class Solution:
     def isUgly(self, n: int) -> bool:
         
         for prime in 2, 3, 5:
-            while n % prime == 0 < n:
+            while n % prime == 0 and (n>0):
                 n /= prime
 
         return n == 1
